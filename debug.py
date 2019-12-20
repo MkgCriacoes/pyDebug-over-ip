7677c501e8662f07ad99893dc052102b5705b1ae
Markus Luan <markus23_luan@hotmail.com>
2019-12-20 18:02:46 -0300

    Vizualizar pelo browser

--- debug.py
+++ debug.py
@@ -1,5 +1,4 @@
 import builtins
-#import urllib.request
 import requests
 
 __print = print
@@ -7,10 +6,9 @@ __print = print
 def print(msg):
     __print(str(msg))
     
-    #urllib.request.urlopen("localhost/msg/" + str(msg))
     host = "localhost:8080"
 
-    requests.get("http://" + host + "?msg=" + str(msg))
+    requests.get("http://" + host + "/sendlog?msg=" + str(msg))
 
 builtins.print = print
 
--- debug_server.py
+++ debug_server.py
@@ -6,10 +6,25 @@ import logging
 log = logging.getLogger("werkzeug")
 log.setLevel(logging.ERROR)
 
-@app.route("/")
+logs = ""
+
+@app.route("/sendlog")
 def msg():
+    global logs
     print("Debug: " + request.args.get("msg"))
+
+    logs = "Debug: " + request.args.get("msg") + "<br>" + logs
     return "Ok"
+
+@app.route("/")
+def home():
+    global logs
+    return """<script>
+                setTimeout(function(){
+                    location.reload();
+                }, 500)
+              </script>
+              <h3>""" + logs + """</h3>"""
 
 if __name__ == "__main__":
     ip = "0.0.0.0"
