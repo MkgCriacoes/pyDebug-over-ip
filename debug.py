bf65596b0a9dbf7f6858ff55379cc7809a60c35b
Markus Luan <markus23_luan@hotmail.com>
2019-12-21 14:39:35 -0300

    Menos lag

--- debug.py
+++ debug.py
@@ -1,14 +1,27 @@
 import builtins
 import requests
+import threading
 
 __print = print
+
+class threadEnviar(threading.Thread):
+    msg = None
+    host = None
+
+    def __init__(self, host, msg):
+        threading.Thread.__init__(self)
+        self.host = host
+        self.msg = msg
+
+    def run(self):
+        requests.get("http://" + self.host + "/sendlog?msg=" + str(self.msg))
 
 def print(msg):
     __print(str(msg))
     
     host = "localhost:8080"
 
-    requests.get("http://" + host + "/sendlog?msg=" + str(msg))
+    threadEnviar(host, msg).start()
 
 builtins.print = print
 
